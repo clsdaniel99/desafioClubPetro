fafd48cca4168374ddb60628722096477cb6bd03
clsdaniel99 <carlosdaniel_1999@live.com>
2021-09-16 15:30:34 -0300

    Wip

--- cypress/integration/desafioClubPetro.spec.js
+++ cypress/integration/desafioClubPetro.spec.js
@@ -18,23 +18,20 @@ describe('Desafio ClubPetro', () => {
         cy.scrollTo(0, 2000)
         cy.contains("2021 ClubPetro. Todos os direitos reservados.") //Validação do copyright
         cy.get('#menu-item-87372').click()
-        
-    });
-    it.only('Validação e preenchimento do formulário', () => {
-        cy.visit('https://clubpetro.com/contato/')
+
+        //Preenchimento do formulário
         cy.url().should('include', '/contato') //Validação da URL: https://clubpetro.com/contato/
-        cy.get('#fullname').type('João Roberto Faria')
-        cy.get('#email').type('joaoroberto@teste.com')
-        cy.get('#phone').type(84375654003)
-        cy.get('[data-name=relacao_com_posto2]').select('Dono(a) ou Diretor(a)')
-        cy.get('[data-name=nome_da_rede]').type('Posto Teste')
-        cy.get('[data-name=cnpj]').type(28473880000132)
-        cy.get('[data-name=expectativa_com_clubpetro]').type('Melhor gerenciamento do posto')
-        cy.get('._field99 > ._form-fieldset > ._checkbox-radio').contains("Ao informar meus dados").click()
-        cy.get('._field100 > ._form-fieldset > ._checkbox-radio').contains("Desejo").click()
-        cy.get('button[type="submit"]').click()
+        cy.get('#fullname').type('João Roberto Faria', {force:true})
+        cy.get('#email').type('joaoroberto@teste.com', {force:true})
+        cy.get('#phone').type(84375654003, {force:true})
+        cy.get('[data-name=relacao_com_posto2]').select('Dono(a) ou Diretor(a)', {force:true})
+        cy.get('[data-name=nome_da_rede]').type('Posto Teste', {force:true})
+        cy.get('[data-name=cnpj]').type(28473880000132, {force:true})
+        cy.get('[data-name=expectativa_com_clubpetro]').type('Melhor gerenciamento do posto', {force:true})
+        cy.get('._field99 > ._form-fieldset > ._checkbox-radio').contains("Ao informar meus dados").click({force:true})
+        cy.get('._field100 > ._form-fieldset > ._checkbox-radio').contains("Desejo").click({force:true})
         //cy.get('button[type="submit"]').click()
         //Na realização do teste me deparei com um erro de redirecionamento no framework. 
-        //Pesquisei e não encontrei uma solução para o erro, muitas pessoas que utilizam o framework sofrem do mesmo erro
-        });
+        //Pesquisei e não encontrei uma solução para o erro, muitas pessoas que utilizam o framework sofrem do mesmo erro        
+    });
 });
